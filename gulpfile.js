61efe60b5c23fdd922d584e68ec5dc0e0a1e5dd9
Augusto Llopis <augustollopisar@gmail.com>
2025-08-08 18:09:21 +0100

    Version Final

--- gulpfile.js
+++ gulpfile.js
@@ -1,110 +1,100 @@
-import path from 'path'
-import fs from 'fs'
-import { glob } from 'glob'
-import { src, dest, watch, series } from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
-const sass = gulpSass(dartSass)
+import fs from 'fs';
+import path from 'path';
+import { glob } from 'glob';
+import { src, dest, watch, series, parallel } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import terser from 'gulp-terser';
+import sharp from 'sharp';
 
-import terser from 'gulp-terser'
-import sharp from 'sharp'
+const sass = gulpSass(dartSass);
 
-// JS
-export function js(done) {
-  src('src/js/app.js')
+// ---------- CLEAN ----------
+export function clean(done) {
+  try { fs.rmSync('build', { recursive: true, force: true }); } catch (_) {}
+  fs.mkdirSync('build', { recursive: true });
+  done();
+}
+
+// ---------- JS ----------
+export function js() {
+  return src('src/js/app.js')
     .pipe(terser())
-    .pipe(dest('build/js'))
-  done()
+    .pipe(dest('build/js'));
 }
 
-// CSS
-export function css(done) {
-  src('src/scss/app.scss', { sourcemaps: true })
+// ---------- CSS ----------
+export function css() {
+  return src('src/scss/app.scss', { sourcemaps: true })
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(dest('build/css', { sourcemaps: '.' }))
-  done()
+    .pipe(dest('build/css', { sourcemaps: '.' }));
 }
 
-// (Opcional) Thumbnails de galería – desactivado para evitar errores
-export async function crop(done) {
-  const inputFolder = 'src/img/gallery/full'
-  const outputFolder = 'src/img/gallery/thumb'
-  const width = 250
-  const height = 180
-
-  if (!fs.existsSync(outputFolder)) {
-    fs.mkdirSync(outputFolder, { recursive: true })
-  }
-
-  const images = fs.readdirSync(inputFolder).filter(file => /\.(jpg)$/i.test(path.extname(file)))
-  try {
-    for (const file of images) {
-      const inputFile = path.join(inputFolder, file)
-      const outputFile = path.join(outputFolder, file)
-      if (!fs.existsSync(outputFile)) {
-        await sharp(inputFile).resize(width, height, { position: 'centre' }).toFile(outputFile)
-      }
-    }
-    done()
-  } catch (error) {
-    console.log(error)
-    done(error)
-  }
-}
-
-// Imágenes a build (solo jpg/png)
+// ---------- IMÁGENES ----------
 export async function imagenes(done) {
-  const srcDir = './src/img'
-  const buildDir = './build/img'
-  const images = await glob('./src/img/**/*.{jpg,png}', { nodir: true })
+  const srcDir = './src/img';
+  const buildDir = './build/img';
+  const images = await glob('./src/img/**/*.{jpg,jpeg,png}', { nodir: true });
 
-  images.forEach(file => {
-    const relativePath = path.relative(srcDir, path.dirname(file))
-    const outputSubDir = path.join(buildDir, relativePath)
-    procesarImagenes(file, outputSubDir)
-  })
-  done()
-}
-
-function procesarImagenes(file, outputSubDir) {
-  if (!fs.existsSync(outputSubDir)) {
-    fs.mkdirSync(outputSubDir, { recursive: true })
+  for (const file of images) {
+    const relativePath = path.relative(srcDir, path.dirname(file));
+    const outDir = path.join(buildDir, relativePath);
+    procesarImagenes(file, outDir);
   }
-  const baseName = path.basename(file, path.extname(file))
-  const extName = path.extname(file)
-  const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
-  const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
-  const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
-
-  const options = { quality: 80 }
-  sharp(file).jpeg(options).toFile(outputFile)
-  sharp(file).webp(options).toFile(outputFileWebp)
-  sharp(file).avif().toFile(outputFileAvif)
+  done();
 }
 
-// HTML a build (raíz + src/)
+function procesarImagenes(file, outDir) {
+  if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
+  const baseName = path.basename(file, path.extname(file));
+  const extName = path.extname(file);
+  const outFile = path.join(outDir, `${baseName}${extName}`);
+  const outWebp = path.join(outDir, `${baseName}.webp`);
+  const outAvif = path.join(outDir, `${baseName}.avif`);
+  const options = { quality: 80 };
+
+  sharp(file).toFile(outFile);
+  sharp(file).webp(options).toFile(outWebp);
+  sharp(file).avif().toFile(outAvif);
+}
+
+// ---------- HTML ----------
 export function html() {
   return src(['*.html', 'src/**/*.html'])
-    .pipe(dest('build'))
+    .pipe(dest('build'));
 }
 
-// Videos a build (copia sin modificar)
-export function videos() {
-  return src('src/video/**/*.{mp4,webm}')
-    .pipe(dest('build/video', { encoding: false }));
+// ---------- COPIA FINAL DE VIDEOS ----------
+export function copyVideoFinal() {
+  const inDir = 'src/video';
+  const outDir = 'build/video';
+
+  if (!fs.existsSync(inDir)) return Promise.resolve();
+  if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
+
+  for (const f of fs.readdirSync(inDir)) {
+    const name = f.toLowerCase();
+    if (name.endsWith('.mp4') || name.endsWith('.webm')) {
+      fs.copyFileSync(`${inDir}/${f}`, `${outDir}/${f}`);
+    }
+  }
+  return Promise.resolve();
 }
 
-// Watch de desarrollo
+// ---------- WATCH ----------
 export function dev() {
-  watch('src/scss/**/*.scss', css)
-  watch('src/js/**/*.js', js)
-  watch('src/img/**/*.{png,jpg}', imagenes)
-  watch('src/video/**/*', videos)
-  watch(['*.html', 'src/**/*.html'], html)
+  watch('src/scss/**/*.scss', css);
+  watch('src/js/**/*.js', js);
+  watch('src/img/**/*.{jpg,jpeg,png}', imagenes);
+  watch(['*.html', 'src/**/*.html'], html);
+  watch('src/video/**/*.{mp4,webm}', copyVideoFinal);
 }
 
-// PRODUCCIÓN (para Netlify y local): sin crop, videos primero
-export const build = series(videos, js, css, imagenes, html)
+// ---------- BUILD Y DEFAULT ----------
+export const build = series(
+  clean,
+  parallel(js, css, imagenes, html),
+  copyVideoFinal
+);
 
-// DESARROLLO (sin crop para evitar paradas)
-export default series(videos, js, css, imagenes, html, dev)
+export default series(build, dev);
